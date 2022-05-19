0a950fc003a9a3d8b7b32fcb66d58fc5979f5942
Torben Knerr <mail@tknerr.de>
2022-05-20 00:57:22 +0200

    fix type in virtualbox test

--- test/virtualbox/virtualbox_sh_spec.rb
+++ test/virtualbox/virtualbox_sh_spec.rb
@@ -1,6 +1,6 @@
 require_relative "../spec_helper"
 
-describe "virtualbox template" do
+describe "virtualbox.sh" do
   it "should install Virtualbox Guest Tools" do
     expect(user_command("lsmod | grep vboxguest").stdout).to match(/vboxguest/)
   end
